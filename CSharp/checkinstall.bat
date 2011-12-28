ca474ba98279ffb838ef9e931e707e3ed3328ceb
joshbirk <joshua.birk@salesforce.com>
2011-12-28 13:27:54 -0600

    Updating bat scripts to use v4 of the .net framework

--- CSharp/checkinstall.bat
+++ CSharp/checkinstall.bat
@@ -1,10 +1,10 @@
 ECHO Checking for csc.exe compiler.
 
-IF EXIST C:\windows\microsoft.net\Framework\v2.0.50727\csc.exe (
+IF EXIST C:\windows\microsoft.net\Framework\v4.0.30319\csc.exe (
 	ECHO SUCCESS: csc.exe file found.
 ) ELSE (
 	ECHO ERROR: csc.exe file not found, opening Internet Explorer.
-	start iexplore "http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=19"
+	start iexplore "http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=17718"
 )
 
 PAUSE
